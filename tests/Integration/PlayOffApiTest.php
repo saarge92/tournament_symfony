76d2c81866cdfddd3ec5a07541eca7625e5a1041
Serdar Dudryev <sarage92@mail.ru>
2021-04-25 11:07:58 +0400

    Playoff test finished

--- tests/Integration/PlayOffApiTest.php
+++ tests/Integration/PlayOffApiTest.php
@@ -2,13 +2,19 @@
 
 namespace App\Tests\Integration;
 
+use App\Entity\Tournament;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class PlayOffApiTest extends WebTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
     public function testNotTournamentException(): void
     {
-        $client = static::createClient();
+        $client = $this->createClient();
         $client->catchExceptions(true);
         $client->request('GET', '/api/playoff/tournament/-1');
 
@@ -16,5 +22,23 @@ class PlayOffApiTest extends WebTestCase
         $code = $response->getStatusCode();
 
         $this->assertEquals(409, $code);
+    }
+
+    public function testGenerateTournamentPlayoff(): void
+    {
+        $client = $this->createClient();
+        $tournamentRepository = self::$container->get('doctrine')->getRepository(Tournament::class);
+        $randomTournament = $tournamentRepository->getRandomTournament();
+        $this->assertNotNull($tournamentRepository);
+
+        $client->request('POST', "/api/playoff/tournament/" . $randomTournament->getId() . "/generate");
+        $response = $client->getResponse();
+        $body = json_decode($response->getContent(), true);
+
+        $this->assertSame(200, $response->getStatusCode());
+        $responsesKeysToCheck = ['id_tournament', 'tournament_name', 'quarter_final', 'semifinal', 'final_tournament'];
+        foreach ($responsesKeysToCheck as $responseKey) {
+            $this->assertArrayHasKey($responseKey, $body);
+        }
     }
 }
